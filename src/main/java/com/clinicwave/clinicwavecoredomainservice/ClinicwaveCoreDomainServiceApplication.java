1e54fa50a2550f95e7657c03b7edd3e6b50ad825
aamirxshaikh <aamirshaikh3232@gmail.com>
2024-05-27 19:10:35 +0530

    Initialize Spring Boot project with data jpa and actuator dependencies

---
+++ src/main/java/com/clinicwave/clinicwavecoredomainservice/ClinicwaveCoreDomainServiceApplication.java
@@ -0,0 +1,13 @@
+package com.clinicwave.clinicwavecoredomainservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicwaveCoreDomainServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicwaveCoreDomainServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clinicwave/clinicwavecoredomainservice/ClinicwaveCoreDomainServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinicwave.clinicwavecoredomainservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicwaveCoreDomainServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

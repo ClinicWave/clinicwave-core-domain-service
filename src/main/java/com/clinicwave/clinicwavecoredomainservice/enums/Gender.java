8f57a68e5ddd8aa9aa98bf0549845b80e9456d23
aamirxshaikh <aamirshaikh3232@gmail.com>
2024-05-30 21:10:30 +0530

    Add Gender Enum and ClinicWaveUser Entity
    This commit introduces the following changes to the codebase:
    
    - Adds a new Gender enum to represent male and female genders
    - Adds a new ClinicWaveUser entity class to represent a user in the application
    - The ClinicWaveUser entity has properties such as firstName, lastName, username, email, dateOfBirth, gender, bio, status, and a one-to-one relationship with the Role entity

---
+++ src/main/java/com/clinicwave/clinicwavecoredomainservice/enums/Gender.java
@@ -0,0 +1,9 @@
+package com.clinicwave.clinicwavecoredomainservice.enums;
+
+/**
+ * @author aamir on 5/29/24
+ */
+public enum Gender {
+  MALE,
+  FEMALE
+}
---
+++ src/main/java/com/clinicwave/clinicwavecoredomainservice/user/ClinicWaveUser.java
@@ -0,0 +1,48 @@
+package com.clinicwave.clinicwavecoredomainservice.user;
+
+import com.clinicwave.clinicwavecoredomainservice.audit.Audit;
+import com.clinicwave.clinicwavecoredomainservice.enums.Gender;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+/**
+ * @author aamir on 5/29/24
+ */
+@Entity
+@Table(name = "ClinicWaveUser")
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class ClinicWaveUser extends Audit implements Serializable {
+  @Serial
+  private static final long serialVersionUID = 1L;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  private String firstName;
+
+  private String lastName;
+
+  private String username;
+
+  private String email;
+
+  private LocalDate dateOfBirth;
+
+  private Gender gender;
+
+  private String bio;
+
+  private Boolean status;
+
+  @OneToOne
+  private Role role;
+}
